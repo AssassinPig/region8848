c4efa22b2293885aa9c13c48f005ef27886f281b
AssassinPig <AssassinPig@gmail.com>
2014-01-26 14:33:46 +0800

    add icon

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,10 @@
 module CategoriesHelper
+  def get_category_tag(id)
+    category = Category.find(id)
+    Rails.logger.debug(category)
+    if category.nil?
+      category.name = "default category" 
+    end
+    category.name
+  end
 end
